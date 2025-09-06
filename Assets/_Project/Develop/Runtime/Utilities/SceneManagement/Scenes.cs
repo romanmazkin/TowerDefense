5a241a71e491cbbad07f4c6acf9f14d9de28ff6c
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-06 17:41:00 +0300

    Create Scenes

---
+++ Assets/_Project/Develop/Runtime/Utilities/SceneManagement/Scenes.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets._Project.Develop.Runtime.Utilities.SceneManagement
+{
+    public class Scenes
+    {
+        public const string GameEntryPoint = "GameEntryPoint";
+        public const string Empty = "Empty";
+        public const string MainMenu = "MainMenu";
+        public const string Gameplay = "Gameplay";
+    }
+}
