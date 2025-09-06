b6b0073e69c07b14c148713516700520f7480c9e
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-06 16:18:53 +0300

    Delete test objects

--- Assets/_Project/Develop/Runtime/Utilities/ConfigsManagement/ResourcesConfigsLoader.cs
+++ Assets/_Project/Develop/Runtime/Utilities/ConfigsManagement/ResourcesConfigsLoader.cs
@@ -19,7 +19,6 @@ namespace Assets._Project.Develop.Runtime.Utilities.ConfigsManagement
 
         private readonly Dictionary<Type, string> _configsResourcesPaths = new()
         {
-            {typeof(TestConfig), "TestConfig" }
         };
 
         public ResourcesConfigsLoader(ResourcesAssetsLoader resources)
