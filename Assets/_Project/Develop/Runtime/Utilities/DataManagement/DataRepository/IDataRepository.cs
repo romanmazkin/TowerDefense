200e533ef0187b6eaf3d0b3c5e579368da6f6fd1
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-18 22:11:31 +0300

    Create LocalFileDataRepository

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/DataRepository/IDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace Assets._Project.Develop.Runtime.Utilities.DataManagement.DataRepository
+{
+    public interface IDataRepository
+    {
+        IEnumerator Read(string key, Action<string> onRead);
+        IEnumerator Write(string key, string serializedData);
+        IEnumerator Remove(string key);
+        IEnumerator Exists(string key, Action<bool> onExistsResult);
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/DataRepository/LocalFileDataRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Assets._Project.Develop.Runtime.Utilities.DataManagement.DataRepository
+{
+    public class LocalFileDataRepository : IDataRepository
+    {
+        private readonly string _folderPath;
+        private readonly string _saveFileExtention;
+
+        public LocalFileDataRepository(string folderPath, string saveFileExtention)
+        {
+            _folderPath = folderPath;
+            _saveFileExtention = saveFileExtention;
+        }
+
+        public IEnumerator Exists(string key, Action<bool> onExistsResult)
+        {
+            bool exists = File.Exists(FullPathFor(key));
+            onExistsResult?.Invoke(exists);
+            yield break;
+        }
+
+        public IEnumerator Read(string key, Action<string> onRead)
+        {
+            string text = File.ReadAllText(FullPathFor(key));
+            onRead?.Invoke(text);
+            yield break;
+        }
+
+        public IEnumerator Remove(string key)
+        {
+            File.Delete(FullPathFor(key));
+            yield break;
+        }
+
+        public IEnumerator Write(string key, string serializedData)
+        {
+            File.WriteAllText(FullPathFor(key), serializedData);
+            yield break;
+        }
+
+        private string FullPathFor(string key)
+            => Path.Combine(_folderPath, key) + "." + _saveFileExtention;
+    }
+}
