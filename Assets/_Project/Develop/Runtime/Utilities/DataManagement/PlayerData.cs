82bd7bff2c14a768ac67d71a4fbd4b56afd2a8db
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-18 19:12:29 +0300

    Add NuGetForUnity Plugin

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
+using System.Collections.Generic;
+
+namespace Assets._Project.Develop.Runtime.Utilities.DataManagement
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyTypes, int> WalletData;
+    }
+}
