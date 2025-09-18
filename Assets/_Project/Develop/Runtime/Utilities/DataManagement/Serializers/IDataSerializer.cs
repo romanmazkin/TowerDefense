b78f3d382fef7e32d260c0d711b31cb8755b22a9
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-18 19:24:55 +0300

    Create JsonSerializer

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/Serializers/IDataSerializer.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets._Project.Develop.Runtime.Utilities.DataManagement.Serializers
+{
+    public interface IDataSerializer
+    {
+        string Serialize<TData>(TData data);
+
+        TData Deserialize<TData>(string serializedData);
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/Serializers/JsonSerializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Assets._Project.Develop.Runtime.Utilities.DataManagement.Serializers
+{
+    public class JsonSerializer : IDataSerializer
+    {
+        public TData Deserialize<TData>(string serializedData)
+        {
+             return JsonConvert.DeserializeObject<TData>(serializedData);
+        }
+
+        public string Serialize<TData>(TData data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+    }
+}
