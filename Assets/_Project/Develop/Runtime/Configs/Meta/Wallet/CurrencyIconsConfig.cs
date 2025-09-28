cf11d8b0b3839c2d4cbbdf2d40a4f293c7d1c38d
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-28 16:21:41 +0300

    Create IconTextView + CurrencyIconsConfig

---
+++ Assets/_Project/Develop/Runtime/Configs/Meta/Wallet/CurrencyIconsConfig.cs
@@ -0,0 +1,24 @@
+﻿using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Meta.Wallet
+{
+    [CreateAssetMenu(menuName = "Configs/Meta/Wallet/NewCurrencyIconsConfig", fileName = "CurrencyIconsConfig")]
+    public class CurrencyIconsConfig : ScriptableObject
+    {
+        [SerializeField] private List<CurrencyCongfig> _configs;
+
+        public Sprite GetSpriteFor(CurrencyTypes currencyType)
+            => _configs.First(config => config.Type == currencyType).Sprite;
+
+        [Serializable]
+        private class CurrencyCongfig
+        {
+            [field: SerializeField] public CurrencyTypes Type { get; private set; }
+            [field: SerializeField] public Sprite Sprite { get; private set; }
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/UI/CommonViews/IconTextView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets._Project.Develop.Runtime.UI.CommonViews
+{
+    public class IconTextView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private Image _icon;
+
+        public void SetText(string text) => _text.text = text;
+        public void SetIcon(Sprite icon) => _icon.sprite = icon;
+    }
+}
