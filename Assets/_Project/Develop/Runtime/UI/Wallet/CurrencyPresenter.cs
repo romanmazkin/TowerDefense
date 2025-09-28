03d595a3fde836e73cf66f2c7172f924398e4248
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-09-28 16:31:05 +0300

    Create CurrencyPresenter

---
+++ Assets/_Project/Develop/Runtime/UI/Wallet/CurrencyPresenter.cs
@@ -0,0 +1,52 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Meta.Wallet;
+using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
+using Assets._Project.Develop.Runtime.UI.CommonViews;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Project.Develop.Runtime.UI.Wallet
+{
+    public class CurrencyPresenter
+    {
+        private readonly IReadOnlyVariable<int> _currency;
+        private readonly CurrencyTypes _currencyType;
+        private readonly CurrencyIconsConfig _currencyIconsConfig;
+
+        private readonly IconTextView _view;
+
+        private IDisposable _disposable;
+
+        public CurrencyPresenter(
+            IReadOnlyVariable<int> currency, 
+            CurrencyTypes currencyType, 
+            CurrencyIconsConfig currencyIconsConfig, 
+            IconTextView view)
+        {
+            _currency = currency;
+            _currencyType = currencyType;
+            _currencyIconsConfig = currencyIconsConfig;
+            _view = view;
+        }
+
+        public void Enable()
+        {
+            UpdateValue(_currency.Value);
+            _view.SetIcon(_currencyIconsConfig.GetSpriteFor(_currencyType));
+
+            _disposable = _currency.Subscribe(OnCurrenceChanged);
+        }
+
+        public void Disable()
+        {
+            _disposable.Dispose();
+        }
+
+        private void OnCurrenceChanged(int arg1, int newValue) => UpdateValue(newValue);
+
+        private void UpdateValue(int value) => _view.SetText(value.ToString());
+    }
+}
