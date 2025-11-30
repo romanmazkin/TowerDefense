e4c088e23f617ba9c35b83870294c2beb7e324ac
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-11-30 14:45:11 +0300

    Create LevelsProgressionService

---
+++ Assets/_Project/Develop/Runtime/Meta/Features/LevelsProgression/LevelsProgressionService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Assets._Project.Develop.Runtime.Meta.Features.LevelsProgression
+{
+    public class LevelsProgressionService
+    {
+        private const int FirstLevel = 1;
+
+        private readonly List<int> _completedLevels = new();
+
+        public bool IsLeveCompeted(int levelNumber) => _completedLevels.Contains(levelNumber);
+
+        public void AddLeveToCompeted(int levelNumber)
+        {
+            if(IsLeveCompeted(levelNumber))
+                return; 
+            
+            _completedLevels.Add(levelNumber);
+        }
+
+        public bool CanPay(int leveNumber)
+        {
+            return leveNumber == FirstLevel || PreviousLevelCompleted(leveNumber); 
+        }
+
+        private bool PreviousLevelCompleted(int levelNumber) => IsLeveCompeted(levelNumber - 1);
+    }
+}
