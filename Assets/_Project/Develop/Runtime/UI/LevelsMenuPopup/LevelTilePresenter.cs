352eda07d69a7fb6637ffa996613f0d9b9d24ad2
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-12-02 19:13:21 +0300

    Create LevelTilePresenter

---
+++ Assets/_Project/Develop/Runtime/UI/LevelsMenuPopup/LevelTilePresenter.cs
@@ -0,0 +1,69 @@
+﻿using Assets._Project.Develop.Runtime.Meta.Features.LevelsProgression;
+using Assets._Project.Develop.Runtime.UI.Core;
+using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagement;
+using Assets._Project.Develop.Runtime.Utilities.SceneManagement;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.UI.LevelsMenuPopup
+{
+    public class LevelTilePresenter : IPresenter
+    {
+        private readonly LevelsProgressionService _levelsService;
+        private readonly SceneSwitcherService _sceneSwitcherService;
+        private readonly ICoroutinesPerformer _coroutinesPerformer;
+
+        private readonly int _levelNumber;
+
+        private readonly LevelTileView _view;
+
+        public LevelTilePresenter(
+            LevelsProgressionService levelsService, 
+            SceneSwitcherService sceneSwitcherService, 
+            ICoroutinesPerformer coroutinesPerformer, 
+            int levelNumber, 
+            LevelTileView view)
+        {
+            _levelsService = levelsService;
+            _sceneSwitcherService = sceneSwitcherService;
+            _coroutinesPerformer = coroutinesPerformer;
+            _levelNumber = levelNumber;
+            _view = view;
+        }
+
+        public void Initialize()
+        {
+            _view.SetLevel(_levelNumber.ToString());
+
+            if (_levelsService.CanPay(_levelNumber))
+            {
+                if (_levelsService.IsLeveCompeted(_levelNumber))
+                    _view.SetComplete();
+                else
+                    _view.SetActive();
+            }
+            else
+            {
+                _view.SetBlock();
+            }
+
+            _view.Clicked += OnViewClicked;
+        }
+
+        public void Dispose()
+        {
+            _view.Clicked -= OnViewClicked;
+        }
+
+        private void OnViewClicked()
+        {
+            if(_levelsService.CanPay(_levelNumber) == false)
+            {
+                Debug.Log("Level blocked");
+                return;
+            }
+
+            _coroutinesPerformer
+                .StartPerform(_sceneSwitcherService.ProcessSwitchTo(Scenes.Gameplay, new SceneLoadingData(_levelNumber)));
+        }
+    }
+}
--- Assets/_Project/Develop/Runtime/UI/ProjectPresentersFactory.cs
+++ Assets/_Project/Develop/Runtime/UI/ProjectPresentersFactory.cs
@@ -1,12 +1,15 @@
 ﻿using Assets._Project.Develop.Runtime.Configs.Meta.Wallet;
+using Assets._Project.Develop.Runtime.Meta.Features.LevelsProgression;
 using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
 using Assets._Project.Develop.Runtime.UI.CommonViews;
 using Assets._Project.Develop.Runtime.UI.Core;
 using Assets._Project.Develop.Runtime.UI.Core.TestPopup;
+using Assets._Project.Develop.Runtime.UI.LevelsMenuPopup;
 using Assets._Project.Develop.Runtime.UI.Wallet;
 using Assets._Project.Develop.Runtime.Utilities.ConfigsManagement;
 using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagement;
 using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using Assets._Project.Develop.Runtime.Utilities.SceneManagement;
 using Zenject;
 
 namespace Assets._Project.Develop.Runtime.UI
@@ -17,12 +20,19 @@ namespace Assets._Project.Develop.Runtime.UI
         private WalletService _walletService;
         private ViewsFactory _viewsFactory;
 
+        private LevelsProgressionService _levelsService;
+        private SceneSwitcherService _sceneSwitcherService;
         ICoroutinesPerformer _performer;
 
         [Inject]
-        public void Construct(ICoroutinesPerformer coroutinesPerformer)
+        public void Construct(
+            ICoroutinesPerformer coroutinesPerformer,
+            LevelsProgressionService levelsProgressionService,
+            SceneSwitcherService sceneSwitcherService)
         {
             _performer = coroutinesPerformer; 
+            _levelsService = levelsProgressionService;
+            _sceneSwitcherService = sceneSwitcherService;
         }
 
         public ProjectPresentersFactory(
@@ -55,6 +65,11 @@ namespace Assets._Project.Develop.Runtime.UI
         public TestPopupPresenter CreateTestPopupPresenter(TestPopupView view)
         {
             return new TestPopupPresenter(view, _performer);
+        }
+
+        public LevelTilePresenter CreateLevelTilePresenter(LevelTileView view, int levelNumber)
+        {
+            return new LevelTilePresenter(_levelsService, _sceneSwitcherService, _performer, levelNumber, view);
         }
     }
 }
