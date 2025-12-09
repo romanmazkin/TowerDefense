b990790761eddc76cab5c986a407c919c85c74fa
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-12-09 20:57:54 +0300

    Create EntitiesFactory

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory.cs
@@ -0,0 +1,25 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+using Zenject;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
+{
+    public class EntitiesFactory
+    {
+        [Inject]
+        public void Construct()
+        {
+        }
+
+        public Entity CreateTestEntity()
+        {
+            Entity entity = new Entity();
+
+            entity.AddComponent(new MoveDirection() { Value = new ReactiveVariable<Vector3>(Vector3.forward) });
+            entity.AddComponent(new MoveSpeed() { Value = new ReactiveVariable<float>(10) });
+
+            return entity;
+        }
+    }
+}
