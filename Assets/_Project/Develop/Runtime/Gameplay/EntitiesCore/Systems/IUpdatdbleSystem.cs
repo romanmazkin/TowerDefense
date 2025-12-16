eda1ede83735a12bd1d4942b06d0e137b7d7aa4d
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-12-16 20:13:05 +0300

    Create MovementSystem

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Systems/IUpdatdbleSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Systems
+{
+    public interface IUpdatdbleSystem : IEntitySystem
+    {
+        void OnUpdate(float deltaTimeS);
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/MovementFeature/MovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Systems;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature
+{
+    public class MovementSystem : IInitializableSystem, IUpdatdbleSystem
+    {
+        private Entity _entity;
+
+        public void OnInit(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        public void OnUpdate(float deltaTimeS)
+        {
+           ReactiveVariable<Vector3> moveDirection = _entity.GetComponent<MoveDirection>().Value;
+           ReactiveVariable<float> moveSpeed = _entity.GetComponent<MoveSpeed>().Value;
+
+            Vector3 velocity = moveDirection.Value.normalized * moveSpeed.Value;
+
+            Debug.Log("Speed is " + velocity.ToString());
+        }
+    }
+}
