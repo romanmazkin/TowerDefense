710725efc528d623e66be02d07de73e6517b3878
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2025-12-17 19:09:44 +0300

    Create MonoEntityRegistrator

--- Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Mono/MonoEntity.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Mono/MonoEntity.cs
@@ -6,7 +6,11 @@ namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono
     {
         public void Setup(Entity entity)
         {
+            MonoEntityRegistrator[] registrators = GetComponentsInChildren<MonoEntityRegistrator>();
 
+            if(registrators != null )
+                foreach(MonoEntityRegistrator registrator in registrators)
+                    registrator.Register(entity);
         }
 
         public void Cleanup(Entity entity)
