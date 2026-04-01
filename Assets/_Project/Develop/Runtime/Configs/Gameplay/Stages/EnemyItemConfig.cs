07237db31d21f0ec24a2e5f11d70cf0e8988d337
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2026-04-01 21:37:58 +0300

    Create ClearEnemiesStage configs

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Stages/EnemyItemConfig.cs
@@ -0,0 +1,17 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Stages
+{
+    [Serializable]
+    public class EnemyItemConfig
+    {
+        [field: SerializeField] public Vector3 SpawnPosition { get; private set; }
+        [field: SerializeField] public EntityConfig EnemyConfig { get; private set; }
+    }
+}
