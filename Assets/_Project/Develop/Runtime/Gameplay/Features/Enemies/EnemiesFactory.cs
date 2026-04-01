d21d2b849b9a37e4ac858f18a40b8485e535f056
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2026-04-01 21:17:33 +0300

    Fix NearestDamageableTargetSelector

--- Assets/_Project/Develop/Runtime/Gameplay/Features/Enemies/EnemiesFactory.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Enemies/EnemiesFactory.cs
@@ -44,7 +44,7 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Features.MainHero
                     throw new ArgumentException($"Not support {config.GetType()} type config");
             }
 
-            entity.AddTeam(new ReactiveVariable<Teams>(Teams.Enemies));
+            entity.AddTeam(new ReactiveVariable<Teams>(Teams.MainHero));
 
             _entitiesLifeContext.Add(entity);
 
