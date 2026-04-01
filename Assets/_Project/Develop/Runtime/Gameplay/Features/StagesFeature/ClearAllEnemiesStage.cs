822c103eb3e015a7f2ffe6caa5e148ea0420aae1
romanmazkin <108292277+romanmazkin@users.noreply.github.com>
2026-04-01 21:57:10 +0300

    Done with ClearAllEnemiesStage

--- Assets/_Project/Develop/Runtime/Gameplay/Features/StagesFeature/ClearAllEnemiesStage.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/StagesFeature/ClearAllEnemiesStage.cs
@@ -1,6 +1,9 @@
 ﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Stages;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
 using Assets._Project.Develop.Runtime.Gameplay.Features.MainHero;
 using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using System.Collections.Generic;
 
 namespace Assets._Project.Develop.Runtime.Gameplay.Features.StagesFeature
 {
@@ -12,36 +15,95 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Features.StagesFeature
 
         private EnemiesFactory _enemiesFactory;
 
+        private EntitiesLifeContext _entitiesLifeContext;
+
         private bool _inProcess;
 
+        private Dictionary<Entity, IDisposable> _spawnedEnemiesToRemoveReason = new();
+
         public ClearAllEnemiesStage(
-            ClearAllEnemiesStageConfig config, 
-            EnemiesFactory enemiesFactory)
+            ClearAllEnemiesStageConfig config,
+            EnemiesFactory enemiesFactory,
+            EntitiesLifeContext entitiesLifeContext)
         {
             _config = config;
             _enemiesFactory = enemiesFactory;
+            _entitiesLifeContext = entitiesLifeContext;
         }
 
-        public IReadOnlyEvent Completed => throw new System.NotImplementedException();
+        public IReadOnlyEvent Completed => _completed;
 
         public void Cleanup()
         {
-            throw new System.NotImplementedException();
+            foreach (KeyValuePair <Entity, IDisposable> item in _spawnedEnemiesToRemoveReason)
+            {
+                item.Value.Dispose();
+                _entitiesLifeContext.Release(item.Key);
+            }
+
+            _spawnedEnemiesToRemoveReason.Clear();
+
+            _inProcess = false;
         }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+            foreach (KeyValuePair<Entity, IDisposable> item in _spawnedEnemiesToRemoveReason)
+            {
+                item.Value.Dispose();
+            }
+
+            _spawnedEnemiesToRemoveReason.Clear();
+
+            _inProcess = false;
         }
 
         public void Start()
         {
-            throw new System.NotImplementedException();
+            if (_inProcess)
+                throw new InvalidOperationException("Game mode already started");
+
+            SpawnEnemies();
+
+            _inProcess = true;
         }
 
         public void Update(float deltaTime)
         {
-            throw new System.NotImplementedException();
+            if (_inProcess == false)
+                return;
+
+            if (_spawnedEnemiesToRemoveReason.Count == 0)
+                ProcessEnd();
+        }
+
+        private void ProcessEnd()
+        {
+            _inProcess = false;
+            _completed.Invoke();
+        }
+
+        private void SpawnEnemies()
+        {
+            foreach (EnemyItemConfig enemyItemConfig in _config.EnemyItems)
+                SpawnEnemy(enemyItemConfig);
+        }
+
+        private void SpawnEnemy(EnemyItemConfig enemyItemConfig)
+        {
+            Entity spawnedEnemy = _enemiesFactory.Create(enemyItemConfig.SpawnPosition, enemyItemConfig.EnemyConfig);
+
+            IDisposable removeReason = spawnedEnemy.IsDead.Subscribe((oldValue, isDead) =>
+            {
+                if (isDead)
+                {
+                    IDisposable disposable = _spawnedEnemiesToRemoveReason[spawnedEnemy];
+                    disposable.Dispose();
+                    _spawnedEnemiesToRemoveReason.Remove(spawnedEnemy);
+                }
+            });
+
+            _spawnedEnemiesToRemoveReason.Add(spawnedEnemy, removeReason);
         }
     }
 }
